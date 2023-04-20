69c57bea681b391ad3e8930670ed074d9d3dc652
Kokeb Dese <kokebdese86@gmail.com>
2023-04-20 09:44:12 +0100

    Update hela3d.m

--- hela3d.m
+++ hela3d.m
@@ -1,17 +1,7 @@
 close all
 clear all
-
+%load your 3D data
 load('Hela_ROI_01_30_876_1665_81_Nuclei.mat')
-load('Hela_ROI_01_30_876_1665_81_Cell.mat')
-
-imagesc(Hela_cell(:,:,190)*2+3*Hela_nuclei(:,:,190)+1*Hela_background(:,:,190))
-imagesc(Hela_cell(:,:,90)*2+3*Hela_nuclei(:,:,90)+1*Hela_background(:,:,90))
-
-figure
-h1=isosurface(Hela_nuclei(1:8:end,1:8:end,:),0.5);
-h2=patch(h1);
-h2.EdgeColor='none';
-h2.FaceColor='r';
-lighting phong
-camlight right
+%only considering the first 10 slices
+h1=isosurface(Hela_nuclei(1:10:end,1:10:end,:),0.5);
 stlwrite('hela.stl', h1)
